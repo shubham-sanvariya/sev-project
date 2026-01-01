e99e27e6ee255a7f450d74e31a5dba30490e099b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-01-02 01:35:57 +0530

    Add email verification route: Include verifyEmail endpoint in auth routes

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {asyncHandler} from "../utils/asyncHandler.js";
-import {login, signup} from "../controller/authController.js";
+import {login, signup, verifyEmail} from "../controller/authController.js";
 
 
 const router = express.Router();
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post("/auth/login",asyncHandler(login))
 
 router.post("/auth/signup",asyncHandler(signup))
+
+router.get("/auth/verify-email/:token",asyncHandler(verifyEmail))
 
 export default router;
