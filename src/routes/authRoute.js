8ee87362b5cafaab6775d86e9a37d67daff9ef89
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-01-03 01:23:04 +0530

    Add resend verification route: Include endpoint for resending verification emails with rate limiting

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {asyncHandler} from "../utils/asyncHandler.js";
-import {login, signup, verifyEmail} from "../controller/authController.js";
+import {login, resendVerificationEmail, signup, verifyEmail} from "../controller/authController.js";
+import {resendLimiter} from "../middleware/reqLimiter.js";
 
 
 const router = express.Router();
@@ -10,5 +11,7 @@ router.post("/auth/login",asyncHandler(login))
 router.post("/auth/signup",asyncHandler(signup))
 
 router.get("/auth/verify-email/:token",asyncHandler(verifyEmail))
+
+router.post('/resend-verification', resendLimiter, asyncHandler(resendVerificationEmail));
 
 export default router;
