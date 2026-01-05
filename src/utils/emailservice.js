0e1bfd7a13791233e66bad3ad6c28da3d495199e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2026-01-06 00:14:47 +0530

    refactor: streamline email body generation in sendVerificationEmail function

--- src/utils/emailservice.js
+++ src/utils/emailservice.js
@@ -19,18 +19,42 @@ transporter.verify((error, success) => {
 });
 
 
-export const sendVerificationEmail = async (to, link) => {
-    const name = process.env.SMTP_BREVO_SENDER_NAME
-    const mail = process.env.SMTP_BREVO_SENDER_MAIL
-    const info = await transporter.sendMail({
-        from: `"${name}" <${mail}>`,
-        to,
-        subject: 'Verify your email',
-        html: `
+function getEmailBody(link, uri) {
+    let subject;
+    let html;
+    if (uri === "verify-email") {
+        subject = "Verify your email"
+        html = `
       <h2>Email Verification</h2>
       <p>Click the link below to verify your email:</p>
       <a href="${link}">${link}</a>
     `
+    } else {
+        subject = 'Reset your password'
+        html = `
+            <h2>Password Reset Request</h2>
+            <p>You requested to reset your password.</p>
+            <p>Click the link below to set a new password:</p>
+            <a href="${link}">${link}</a>
+            <p>This link will expire in 15 minutes.</p>
+            <p>If you did not request this, please ignore this email.</p>
+        `
+    }
+
+    return {subject, html};
+}
+
+export const sendVerificationEmail = async (to, link, uri) => {
+    const name = process.env.SMTP_BREVO_SENDER_NAME
+    const mail = process.env.SMTP_BREVO_SENDER_MAIL
+
+    const {html, subject} = getEmailBody(link, uri);
+
+    const info = await transporter.sendMail({
+        from: `"${name}" <${mail}>`,
+        to,
+        subject,
+        html
     });
 
     console.log('📧 Email sent:', info.messageId);
